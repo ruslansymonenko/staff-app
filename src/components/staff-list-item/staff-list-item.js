93979a8b6b8795263ee6688521053ce25afa0675
Ruslan Symonenko <symonenkors@gmail.com>
2023-01-22 12:58:11 +0200

    changed saving state on list item from function to class element, also added functionality of favorite workers

--- src/components/staff-list-item/staff-list-item.js
+++ src/components/staff-list-item/staff-list-item.js
@@ -1,40 +1,65 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './staff-list-item.css';
 
-const StaffListItem = ({name, salary, bonus}) => {
-  const [isBonus, setBonus] = useState(bonus);
+class StaffListItem extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      bonus: false,
+      favorite: false
+    }
+  }
 
-  return (
-    <li 
-      className={`list-group-item d-flex justify-content-between
-        ${
-          isBonus ? 'increase' : ''
-        }`}>
-      <span className="list-group-item-label">{name}</span>
+  handleBonus = () => {
+    this.setState(({bonus}) => ({
+      bonus: !bonus
+    }))
+  }
+
+  handleFavorite = () => {
+    this.setState(({favorite}) => ({
+      favorite: !favorite
+    }))
+  }
+
+  render() {
+    const {name, salary} = this.props;
+    const {bonus, favorite} = this.state;
+
+    let listItemClassLists = `list-group-item d-flex justify-content-between
+    ${bonus ? 'increase' : ''} 
+    ${favorite ? 'like' : ''}`
+
+    return(
+      <li 
+      className={listItemClassLists}>
+      <span 
+        className="list-group-item-label"
+        onClick={this.handleFavorite}>
+        {name}
+        </span>
       <input 
         type="text" 
         className="list-group-item-input" 
         defaultValue={salary + ' $'}/>
       <div className='d-flex justify-content-center align-items-center'>
-          <button type="button"
-              className="btn-cookie btn-sm "
-              onClick={() => {
-                  !isBonus ? setBonus(true) : setBonus(false)
-                }}>
-              <i className="fas fa-cookie"></i>
-          </button>
+        <button type="button"
+            className="btn-cookie btn-sm "
+            onClick={this.handleBonus}>
+            <i className="fas fa-cookie"></i>
+        </button>
 
-          <button 
-                type="button"
-                className="btn-trash btn-sm ">
-              <i className="fas fa-trash"></i>
-          </button>
-          <i 
-            className="fas fa-star">
-            </i>
+        <button 
+              type="button"
+              className="btn-trash btn-sm ">
+            <i className="fas fa-trash"></i>
+        </button>
+        <i className="fas fa-star"></i>
       </div>
     </li>
-  )
+    )
+  }
+
 }
 
 export default StaffListItem;
